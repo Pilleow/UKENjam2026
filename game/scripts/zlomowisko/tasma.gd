b418919888d7d810554bb5c1d9b6041be9616fad
= <janowiakseweryn@gmail.com>
2026-04-11 09:34:43 +0200

    added basic animation

--- game/scripts/zlomowisko/tasma.gd
+++ game/scripts/zlomowisko/tasma.gd
@@ -11,24 +11,47 @@ var koszyk_pos =  Vector2(1000,100);
 
 var zlom = 0;
 #odstep czasowy pomiedzy złomem
-var counter_max = 1.0;
+var counter_max = 1.5;
 var counter = 0;	
 
+
+# 0 - smiec, 1,2,3 - zlom1,2,3 
+#warning -> to nie sa procenty tylko prrogi ktore wyznacza funckja _procentage !!!! (line 39)
+var srap_distribution = [0,0,0,0];
+
+# x0- prawdopodobienstwo smiecia ,x1,x2,x3 - prawd zlom1,2,3 
+func _procentage(x0: float, x1: float, x2: float,x3: float):
+	if x0+x1+x2+x3 != 1:
+		print("Error: zla dystrybucja ");
+		
+	srap_distribution[0] = x0;
+	srap_distribution[1] = x0+x1;
+	srap_distribution[2] = srap_distribution[1]+x2;
+	# to w sumie nie potrzebne XD
+	srap_distribution[3] = srap_distribution[2]+x3;
+
 func _ready() -> void:
+	
+	#tu ustawiasz proceny
+	
+	# x0- prawdopodobienstwo smiecia ,x1,x2,x3 - prawd zlom1,2,3 
+	_procentage(0.3,0.4,0.2,0.1)
+
+
 	randomize()
 
 	# 1. Random Float between 0.0 and 1.0
-
+	
 func _spawn(delta: float) -> void:
 	counter += delta;
 	if counter >= counter_max:
-		var chance = randf()
 		
-		if chance < 0.25:
+		var chance = randf()
+		if chance < srap_distribution[0]:
 			zlom = smiec.instantiate();
-		elif chance < 0.50:
+		elif chance < srap_distribution[1]:
 			zlom = zlom1.instantiate();
-		elif chance < 0.75:
+		elif chance < srap_distribution[2]:
 			zlom = zlom2.instantiate();
 		else:
 			zlom = zlom3.instantiate();
--- game/scripts/zlomowisko/zlom_player.gd
+++ game/scripts/zlomowisko/zlom_player.gd
@@ -1,12 +1,18 @@
 extends CharacterBody2D
 
-@export var speed = 200;
-@export var target_pos = 400;
+@export var speed:float = 200.0;
+@export var comeback_speed: float = 300;
+@export var target_pos: float = 400.0;
+
 var catch = false;
+var move2startpos = false;
+
+#sprawdz czy gracz oddal juz zlom na smietnik lub do plecaka 
+var decision = true;
 
 var start_pos = 0;
 
-var time_to_move_back = 2.0;
+var time_to_move_back = 0.1;
 var current_time = 0;
 		
 func _ready() -> void:
@@ -14,25 +20,39 @@ func _ready() -> void:
 	
 func _move_back(delta:float) -> void:
 	
+	# zlap zlom i poczekaj time_to_move_back czasu
 	if catch:
 		current_time += delta;
 		velocity.y = 0;
-		
+	
 	if current_time >= time_to_move_back:
 		catch = false;
-		velocity.y = speed;
+		velocity.y = comeback_speed;
+		move2startpos = true;
+	
+	#wroc z zlomem do zadanej pozycji	
+	if move2startpos:
+		if position.y >= start_pos:
+			move2startpos = false;
+			current_time = 0;
+			velocity.y = 0;
+			position.y = start_pos;
 		
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("m_left"):
-		print("clicked!")
+	
+
+	if Input.is_action_just_pressed("m_left") and decision:
+		#print("clicked!")
 		velocity.y = -speed;
 		
 	#velocity.y = velocity.move_toward(velocity.y,spe)
 	
-	if position.y <= target_pos:
-		
-		position.y = start_pos;
-		velocity.y = 0;
+	if position.y <= target_pos :
+		catch = true;
+		#position.y = start_pos;
+		#velocity.y = 0;
+		#
+	_move_back(delta)
 		
 	move_and_slide()
 		
